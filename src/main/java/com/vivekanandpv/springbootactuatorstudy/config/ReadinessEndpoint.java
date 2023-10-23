459b7993b0fcf2441c1345f6369a1ed20fbb469f
Vivekanand P V <vivekanandpv@outlook.com>
2023-10-23 14:28:41 +0530

    done

--- src/main/java/com/vivekanandpv/springbootactuatorstudy/config/ReadinessEndpoint.java
+++ src/main/java/com/vivekanandpv/springbootactuatorstudy/config/ReadinessEndpoint.java
@@ -3,7 +3,6 @@ package com.vivekanandpv.springbootactuatorstudy.config;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
