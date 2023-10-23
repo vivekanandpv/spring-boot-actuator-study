49ecf8733c0381fe2aa5ebcb5e6fceae205487bd
Vivekanand P V <vivekanandpv@outlook.com>
2023-10-23 11:00:04 +0530

    done

---
+++ src/main/java/com/vivekanandpv/springbootactuatorstudy/SpringBootActuatorStudyApplication.java
@@ -0,0 +1,13 @@
+package com.vivekanandpv.springbootactuatorstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootActuatorStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vivekanandpv/springbootactuatorstudy/SpringBootActuatorStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivekanandpv.springbootactuatorstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
